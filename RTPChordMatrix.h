f665466c591a3a296a27289c4f676861ac6b7072
Nasker <xamps23@gmail.com>
2022-12-07 12:32:40 +0100

    Added chord reordering function and extended chord intervals assisted by GPT3

--- RTPChordMatrix.h
+++ RTPChordMatrix.h
@@ -48,7 +48,53 @@
       {0,4,8},  //Dominant Ninth
       {0,3,8},  //Dominant Ninth
       {0,6,10} //Mystic
+      /*
+      {"major", {4, 7}},
+      {"minor", {3, 7}},
+      {"diminished", {3, 6}},
+      {"augmented", {4, 8}},
+      {"major seventh", {4, 7, 11}},
+      {"minor seventh", {3, 7, 10}},
+      {"dominant seventh", {4, 7, 10}},
+      {"half-diminished seventh", {3, 6, 10}},
+      {"diminished seventh", {3, 6, 9}},
+      {"augmented seventh", {4, 8, 10}},
+      {"augmented major seventh", {4, 8, 11}},
+      {"sixth", {4, 7, 9}},
+      {"minor sixth", {3, 7, 9}},
+      {"sixth ninth", {4, 7, 9, 14}},
+      {"minor sixth ninth", {3, 7, 9, 13}}
+      */
     };
+
+  /*
+  #include <algorithm>
+#include <vector>
+
+// Reorders the notes in the chord to minimize the distances and finger movements
+// by reordering the notes in the chord to have the same order as in the last chord,
+// starting with the closest note to the root note
+std::vector<int> reorder_chord(std::vector<int> chord, std::vector<int> last_chord) {
+  // Sort the notes in the chord in ascending order
+  std::sort(chord.begin(), chord.end());
+
+  // Find the note in the last chord that is closest to the root note in the current chord
+  int root_note = chord[0];
+  int closest_note = *std::min_element(last_chord.begin(), last_chord.end(), [root_note](int a, int b) {
+    return abs(a - root_note) < abs(b - root_note);
+  });
+
+  // Reorder the notes in the chord to have the same order as in the last chord,
+  // starting with the closest note to the root note
+  std::vector<int> reordered_chord;
+  for (int note : last_chord) {
+    if (note == closest_note) {
+      reordered_chord.insert(reordered_chord.end(), chord.begin(), chord.end());
+    } else {
+      reordered_chord.push_back(note);
+    }
+  }
+  */
 
 class RTPChordMatrix{
   byte _chordType;
